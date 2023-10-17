0448eb67e473ee5efc7ff0f6377f60d01cc2742b
Emre Savaş <emre_savas28@hotmail.com>
2023-10-17 23:46:37 +0300

    Added database connection and displayed data in a DataGridView.

---
+++ Northwind.WebFormsUI/Form1.cs
@@ -0,0 +1,18 @@
+using Northwind.Business.Concrete;
+
+namespace Northwind.WebFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProductManager productManager = new ProductManager();
+            dgwProducts.DataSource = productManager.GetAll();
+        }
+    }
+}
