027a0425ad493890e2f9459de8e413a561c0e78f
Emre Savaş <emre_savas28@hotmail.com>
2023-10-18 20:34:37 +0300

    Implemented repository for data management.

--- Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -3,54 +3,14 @@ using Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Northwind.DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-        public List<Product> GetAll()
-        {
-            using (NorthWindContext context = new NorthWindContext())
-            {
-                return context.Products.ToList();
-            }
-        }
 
-        public Product Get(int id)
-        {
-            using (NorthWindContext context = new NorthWindContext())
-            {
-                return context.Products.SingleOrDefault(p => p.ProductId == id);
-            }
-        }
-
-        public void Add(Product product)
-        {
-            using (NorthWindContext context = new NorthWindContext())
-            {
-                context.Products.Add(product);
-                context.SaveChanges();
-            }
-        }
-
-        public void Update(Product product)
-        {
-            using (NorthWindContext context = new NorthWindContext())
-            {
-                var entity = context.Entry(product);
-                entity.State = System.Data.Entity.EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-        public void Delete(Product product)
-        {
-            using (NorthWindContext context = new NorthWindContext())
-            {
-                context.Products.Remove(product);
-                context.SaveChanges();
-            }
-        }
     }
 }
